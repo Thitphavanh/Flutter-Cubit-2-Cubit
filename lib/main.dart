69e8fa13256d97323b286a5f9fe3f8da4b9f4016
Hery <thitphavanh.psv@gmail.com>
2022-05-24 16:15:39 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cubit_to_cubit/cubits/color/color_cubit.dart';
+import 'package:flutter_cubit_to_cubit/pages/splash_screen_page.dart';
 
 import 'cubits/counter/counter_cubit.dart';
 
@@ -20,64 +21,18 @@ class MyApp extends StatelessWidget {
           create: (context) => ColorCubit(),
         ),
         BlocProvider<CounterCubit>(
-          create: (context) => CounterCubit(
-            colorCubit: context.read<ColorCubit>(),
-          ),
+          create: (context) => CounterCubit(),
         )
       ],
       child: MaterialApp(
         title: 'Cubit2Cubit',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(),
+        home: const SplashScreen(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.watch<ColorCubit>().state.color,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                context.read<ColorCubit>().changeColor();
-              },
-              child: Text(
-                'Change Color',
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              '${context.watch<CounterCubit>().state.counter}',
-              style: TextStyle(
-                fontSize: 52.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            TextButton(
-              onPressed: () {
-                context.read<CounterCubit>().changeCounter();
-              },
-              child: Text(
-                'Increment Counter',
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/pages/splash_screen_page.dart
@@ -0,0 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
+import 'my_home_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Lottie.asset('assets/smear-loader.json'),
+      backgroundColor: Colors.black,
+      nextScreen: const MyHomePage(),
+      splashIconSize: 250,
+      duration: 3000,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+      animationDuration: Duration(seconds: 1),
+    );
+  }
+}
